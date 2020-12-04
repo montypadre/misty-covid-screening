49899bb3d6c4f68809dc4436cb1d1b6dba3da705
Hunter G <32306313+HunterGiannini@users.noreply.github.com>
2020-12-04 15:49:07 -0600

    Add files via upload

---
+++ Misty Covid Screening/mlxCustom.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  Serial.begin(9600);
+  mlx.begin();
+
+}
+
+void loop() {
+  Serial.print(mlx.readAmbientTempF());
+  Serial.print("|");
+  Serial.print(mlx.readObjectTempF());
+  Serial.println();
+  Serial.flush();
+
+}
